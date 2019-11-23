883b6d8827246fcfc49585c4a414db5d4fdd675f
Edwin Muhlestein <Edwin.Muhlestein@Halliburton.com>
2019-11-23 06:39:38 -0600

    initial commit

---
+++ src/main/java/com/intelliviz/BookResource.java
@@ -0,0 +1,27 @@
+package com.intelliviz;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.Collection;
+
+@Path("/") // This is appended to the BASE_URI defined in Main class
+public class BookResource {
+    BookDao dao = new BookDao();
+
+    @Path("/books")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Collection<Book> getBooks() {
+        return dao.getBooks();
+    }
+
+    @Path("/{id}")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Book getBook(@PathParam("id") String id) {
+        return dao.getBook(id);
+    }
+}
